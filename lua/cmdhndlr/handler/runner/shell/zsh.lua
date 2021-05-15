a512ae0293967de434ad14bb2d6e02b3ae21865e
notomo <notomo.motono@gmail.com>
2021-05-15 11:58:21 +0900

    Add zsh handler

---
+++ lua/cmdhndlr/handler/runner/shell/zsh.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.run_file(self, path)
+  return self.job_factory:create({"zsh", path})
+end
+
+function M.run_string(self, str)
+  local path = self.filelib.temporary(str)
+  return self:run_file(path)
+end
+
+return M
