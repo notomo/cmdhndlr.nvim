918841489763990df2ffa88a472f88e98a78ae32
notomo <notomo.motono@gmail.com>
2022-02-09 09:49:08 +0900

    Improve jest test filter

--- lua/cmdhndlr/handler/test_runner/javascript/jest.lua
+++ lua/cmdhndlr/handler/test_runner/javascript/jest.lua
@@ -1,7 +1,12 @@
 local M = {}
 
-function M.run_file(self, _)
-  return self.job_factory:create({ "npx", "jest" })
+function M.run_file(self, path, filter)
+  local cmd = { "npx", "jest" }
+  if filter then
+    table.insert(cmd, "--testNamePattern=" .. filter)
+  end
+  table.insert(cmd, path)
+  return self.job_factory:create(cmd)
 end
 
 M.working_dir = require("cmdhndlr.util").working_dir.upward_pattern("node_modules")
