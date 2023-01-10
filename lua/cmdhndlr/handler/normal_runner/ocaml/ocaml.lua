0a6493a6cbdfb292ed57470648cffd85c6798741
notomo <notomo.motono@gmail.com>
2023-01-10 09:32:37 +0900

    Improve ocaml runner

--- lua/cmdhndlr/handler/normal_runner/ocaml/ocaml.lua
+++ lua/cmdhndlr/handler/normal_runner/ocaml/ocaml.lua
@@ -1,6 +1,17 @@
 local M = {}
 
+M.opts = {
+  use_in_repl = false,
+}
+
 function M.run_file(self, path)
+  if self.opts.use_in_repl then
+    local input = ([[
+#use "%s";;
+#quit;;
+]]):format(path)
+    return self.job_factory:create({ "ocaml" }, { input = input })
+  end
   return self.job_factory:create({ "ocaml", path })
 end
 
