4bee8c69a6a828e5ff84f868211e6f42e4b8d4ce
notomo <notomo.motono@gmail.com>
2023-02-02 21:06:01 +0900

    Add mongosh

---
+++ lua/cmdhndlr/handler/normal_runner/javascript/mongosh.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+function M.run_file(self, path)
+  local input, err = self.filelib.read_all(path)
+  if err then
+    require("cmdhndlr.vendor.misclib.message").error(err)
+  end
+  input = input .. "\nexit;\n"
+  return self.job_factory:create({ "mongosh" }, { input = input })
+end
+
+return M
--- lua/cmdhndlr/lib/file.lua
+++ lua/cmdhndlr/lib/file.lua
@@ -40,4 +40,17 @@ function M.escape(path)
   return ([[`='%s'`]]):format(path:gsub("'", "''"))
 end
 
+function M.read_all(path)
+  local f = io.open(path, "r")
+  if not f then
+    return nil, "cannot read: " .. path
+  end
+  if vim.fn.isdirectory(path) == 1 then
+    return nil, "directory: " .. path
+  end
+  local str = f:read("*a")
+  f:close()
+  return str, nil
+end
+
 return M
