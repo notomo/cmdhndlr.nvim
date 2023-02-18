a6aa1425c796635e791ea24b8519016b8095453b
notomo <notomo.motono@gmail.com>
2023-02-18 11:06:02 +0900

    Update vendor

--- lua/cmdhndlr/handler/test_runner/go/go.lua
+++ lua/cmdhndlr/handler/test_runner/go/go.lua
@@ -1,9 +1,10 @@
 local M = {}
 
-function M.run_file(self, _, filter)
+function M.run_file(self, _, filter, is_leaf)
   local cmd = { "go", "test", "-v" }
   if filter then
-    table.insert(cmd, "--run=" .. ("^%s$"):format(filter))
+    local suffix = is_leaf and "$" or ""
+    table.insert(cmd, "--run=" .. ("^%s%s"):format(filter, suffix))
   end
   return self.job_factory:create(cmd)
 end
