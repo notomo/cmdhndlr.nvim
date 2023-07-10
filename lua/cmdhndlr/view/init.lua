cbdfbec3186ec7bf02dc0717caa3e38b9de9bb54
notomo <notomo.motono@gmail.com>
2023-07-11 07:47:29 +0900

    Small refactor

--- lua/cmdhndlr/view/init.lua
+++ lua/cmdhndlr/view/init.lua
@@ -1,7 +1,6 @@
 local Layout = require("cmdhndlr.view.layout")
 
 local M = {}
-M.__index = M
 
 function M.open(bufnr, working_dir, layout_opts)
   vim.validate({
@@ -17,10 +16,6 @@ function M.open(bufnr, working_dir, layout_opts)
   vim.bo[bufnr].filetype = "cmdhndlr"
   Layout.new(layout_opts):open(bufnr)
   working_dir:set_current()
-
-  local window_id = vim.api.nvim_get_current_win()
-  local tbl = { _bufnr = bufnr, _window_id = window_id }
-  return setmetatable(tbl, M)
 end
 
 return M
