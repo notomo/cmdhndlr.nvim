a3c317d5ab531ba1089e14d490e7b844f98d7e0d
notomo <notomo.motono@gmail.com>
2023-11-09 21:39:17 +0900

    Enable to build on go test file

--- lua/cmdhndlr/handler/build_runner/go/go.lua
+++ lua/cmdhndlr/handler/build_runner/go/go.lua
@@ -1,7 +1,10 @@
 local M = {}
 
-function M.build(ctx, _)
+function M.build(ctx, path)
   local temp = require("cmdhndlr.lib.file").temporary()
+  if vim.endswith(path, "_test.go") then
+    return ctx.job_factory:create({ "go", "test", "-c", "-o", temp })
+  end
   return ctx.job_factory:create({ "go", "build", "-o", temp })
 end
 
