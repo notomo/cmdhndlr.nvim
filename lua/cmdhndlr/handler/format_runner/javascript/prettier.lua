08e317baead0070f0b7f52b5aebee2afa4817364
notomo <notomo.motono@gmail.com>
2023-11-14 21:22:47 +0900

    Use npx for prettier

--- lua/cmdhndlr/handler/format_runner/javascript/prettier.lua
+++ lua/cmdhndlr/handler/format_runner/javascript/prettier.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.format(ctx, path, stdout_collector)
   local content = require("cmdhndlr.lib.file").read_all(path)
-  return ctx.job_factory:create({ "prettier", "--stdin-filepath", path }, {
+  return ctx.job_factory:create({ "npx", "prettier", "--stdin-filepath", path }, {
     input = content,
     on_stdout = stdout_collector,
     as_job = true,
--- lua/cmdhndlr/handler/format_runner/json/prettier.lua
+++ lua/cmdhndlr/handler/format_runner/json/prettier.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.format(ctx, path, stdout_collector)
   local content = require("cmdhndlr.lib.file").read_all(path)
-  return ctx.job_factory:create({ "prettier", "--parser", "json", "--stdin-filepath", path }, {
+  return ctx.job_factory:create({ "npx", "prettier", "--parser", "json", "--stdin-filepath", path }, {
     input = content,
     on_stdout = stdout_collector,
     as_job = true,
