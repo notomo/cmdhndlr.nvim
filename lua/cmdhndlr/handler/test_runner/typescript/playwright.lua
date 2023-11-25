588b8a9c127a87473ba02b0709354cef6f8690cd
notomo <notomo.motono@gmail.com>
2023-11-25 14:11:31 +0900

    Add playwright test runner

---
+++ lua/cmdhndlr/handler/test_runner/typescript/playwright.lua
@@ -0,0 +1,30 @@
+local M = {}
+
+function M.run_file(ctx, path)
+  local cmd = {
+    "npx",
+    "playwright",
+    "test",
+    path,
+  }
+
+  local config_path = ctx.working_dir:marker()
+  if config_path then
+    vim.list_extend(cmd, { "--config", config_path })
+  end
+
+  local extra_args = ctx.opts.extra_args
+  if extra_args then
+    vim.list_extend(cmd, extra_args)
+  end
+
+  return ctx.job_factory:create(cmd)
+end
+
+M.working_dir_marker = require("cmdhndlr.util.working_dir").upward_marker("playwright.config.ts")
+
+M.opts = {
+  extra_args = {},
+}
+
+return M
