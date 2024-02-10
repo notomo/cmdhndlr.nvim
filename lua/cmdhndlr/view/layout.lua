6ee35db687713718d7c100b0479c7af1790f174a
notomo <notomo.motono@gmail.com>
2024-02-10 16:46:09 +0900

    Add tab_drop layout

--- lua/cmdhndlr/view/layout.lua
+++ lua/cmdhndlr/view/layout.lua
@@ -1,40 +1,45 @@
 local Layouts = {}
 
-function Layouts.no() end
-
-function Layouts.horizontal()
-  return function()
-    vim.cmd.split({ mods = { split = "botright" } })
-  end
+function Layouts.no(bufnr)
+  vim.api.nvim_win_set_buf(0, bufnr)
 end
 
-function Layouts.tab()
+function Layouts.horizontal(bufnr)
+  vim.cmd.split({ mods = { split = "botright" } })
+  vim.api.nvim_win_set_buf(0, bufnr)
+end
+
+function Layouts.tab(bufnr)
   vim.cmd.tabedit()
   vim.bo.buftype = "nofile"
   vim.bo.bufhidden = "wipe"
+  vim.api.nvim_win_set_buf(0, bufnr)
+end
+
+function Layouts.tab_drop(bufnr)
+  local window_id = vim.fn.win_findbuf(bufnr)[1]
+  if not window_id then
+    Layouts.tab(bufnr)
+    return
+  end
+
+  vim.api.nvim_set_current_win(window_id)
 end
 
 local Layout = {}
 
 function Layout.open(bufnr, opts)
   vim.validate({ bufnr = { bufnr, "number" } })
-
   opts = opts or {}
-  local typ = opts.type
 
-  local f
-  if typ == "horizontal" then
-    f = Layouts.horizontal()
-  elseif typ == "tab" then
-    f = Layouts.tab
-  elseif typ == "no" then
-    f = Layouts.no
-  else
+  local typ = opts.type
+  local f = Layouts[typ]
+  if not f then
     error("unexpected layout type: " .. tostring(typ))
   end
 
-  f()
-  vim.api.nvim_win_set_buf(0, bufnr)
+  f(bufnr)
+
   return vim.api.nvim_get_current_win()
 end
 
