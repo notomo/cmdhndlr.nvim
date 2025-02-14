0df7abe6ca3e8fff88a0dc14036607b183f56c55
notomo <notomo.motono@gmail.com>
2025-02-14 21:00:02 +0900

    Add cargo test filter

--- lua/cmdhndlr/handler/test_runner/rust/cargo.lua
+++ lua/cmdhndlr/handler/test_runner/rust/cargo.lua
@@ -1,7 +1,11 @@
 local M = {}
 
-function M.run_file(ctx, _)
-  return ctx.job_factory:create({ "cargo", "test" })
+function M.run_file(ctx, _, filter)
+  local cmd = { "cargo", "test" }
+  if filter then
+    table.insert(cmd, filter)
+  end
+  return ctx.job_factory:create(cmd)
 end
 
 M.working_dir_marker = require("cmdhndlr.util.working_dir").upward_marker("Cargo.toml")
