7112afed5d0f2e215fba0846757ff523c1af8e14
notomo <notomo.motono@gmail.com>
2025-02-16 20:53:42 +0900

    Add vitest

---
+++ lua/cmdhndlr/handler/test_runner/javascript/vitest.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+M.opts = {
+  cmd = { "npx", "vitest", "run" },
+  extra_args = {},
+}
+
+function M.run_file(ctx, path, filter, is_leaf)
+  local cmd = {}
+
+  vim.list_extend(cmd, ctx.opts.cmd)
+
+  if filter then
+    local suffix = is_leaf and "$" or ""
+    table.insert(cmd, "--testNamePattern=" .. filter .. suffix)
+  end
+
+  vim.list_extend(cmd, ctx.opts.extra_args)
+
+  table.insert(cmd, vim.fn.escape(path, "[]()$"))
+  return ctx.job_factory:create(cmd)
+end
+
+M.working_dir = require("cmdhndlr.util.working_dir").upward_pattern("vitest.config.ts")
+
+return M
