2c652bd6555cb816d096ed86727b4c3f3fc72b57
notomo <notomo.motono@gmail.com>
2025-11-24 10:50:00 +0900

    Use node --run

--- lua/cmdhndlr/handler/normal_runner/javascript/npm.lua
+++ lua/cmdhndlr/handler/normal_runner/javascript/npm.lua
@@ -3,7 +3,7 @@ local M = {}
 M.opts = { target = "start" }
 
 function M.run_file(ctx, _)
-  return ctx.job_factory:create({ "npm", "run", ctx.opts.target })
+  return ctx.job_factory:create({ "node", "--run", ctx.opts.target })
 end
 
 M.working_dir_marker = require("cmdhndlr.util.working_dir").upward_marker("package.json")
