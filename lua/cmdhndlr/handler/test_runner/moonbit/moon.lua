242b0ebf123680a08b546b8e602044df2934b905
notomo <notomo.motono@gmail.com>
2026-01-18 13:32:27 +0900

    Add moon test filter

--- lua/cmdhndlr/handler/test_runner/moonbit/moon.lua
+++ lua/cmdhndlr/handler/test_runner/moonbit/moon.lua
@@ -1,7 +1,10 @@
 local M = {}
 
-function M.run_file(ctx, path)
+function M.run_file(ctx, path, filter)
   local cmd = { "moon", "test", "-v", path }
+  if filter then
+    vim.list_extend(cmd, { "--filter", filter })
+  end
   return ctx.job_factory:create(cmd)
 end
 
